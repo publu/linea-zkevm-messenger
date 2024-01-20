17f671485c1f3528125c73da9dc741a559ec8479
Pablo <pablo@hashingsystems.com>
2024-01-20 15:46:11 -0600

    better inclusion logic

--- scripts/waitForInclusion.js
+++ scripts/waitForInclusion.js
@@ -2,28 +2,39 @@ const hre = require('hardhat')
 require('dotenv').config()
 
 async function main() {
-  const l1TxHash = "0x4136862000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000184c657420796f75722062616c646e657373207368696e652e000000000000000000000000000000000000000000000000000000000000000000000000"
-  const l2BridgeAddress = '0xC499a572640B64eA1C8c194c43Bc3E19940719dC'
-  
+  const l1TxHash = "0xF3FC26266E40F948424BE972610FCFEED3F6FDDCF27C15362A9A124AEC8D7EB0";
+  const l2BridgeAddress = '0xC499a572640B64eA1C8c194c43Bc3E19940719dC';
+
   // Simpler ABI just for checking the inbox message status
   const bridgeAbi = [
     "function inboxL1L2MessageStatus(bytes32 messageHash) view returns (uint8)"
   ];
-  
-  const signer = await hre.ethers.getSigner()
-  const bridgeContract = new hre.ethers.Contract(l2BridgeAddress, bridgeAbi, signer)
 
-  const messageHash = hre.ethers.utils.solidityKeccak256(['bytes32'], [l1TxHash])
+  const signer = await hre.ethers.getSigner();
+  const bridgeContract = new hre.ethers.Contract(l2BridgeAddress, bridgeAbi, signer);
 
-  const checkInclusion = async () => {
-    const status = await bridgeContract.inboxL1L2MessageStatus(messageHash)
-    if (status === 1) {
-      console.log('The message has been included.')
-      clearInterval(checkInterval)
-    }
-  }
+  const messageHash = hre.ethers.utils.solidityKeccak256(['bytes32'], [l1TxHash]);
 
-  const checkInterval = setInterval(checkInclusion, 10000)
+  await new Promise((resolve, reject) => {
+    const checkInclusion = async () => {
+      try {
+        const status = await bridgeContract.inboxL1L2MessageStatus(messageHash);
+        if (status === 1) {
+          console.log('The message has been included.');
+          clearInterval(checkInterval);
+          resolve();
+        } else {
+          console.log("The message hasn't been included yet. Current Status: ", status)
+        }
+      } catch (error) {
+        console.error('Error checking message status:', error);
+        clearInterval(checkInterval);
+        reject(error);
+      }
+    };
+
+    const checkInterval = setInterval(checkInclusion, 10000);
+  });
 }
 
 
